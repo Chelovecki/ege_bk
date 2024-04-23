7573553e6637b0587ef02a3e8860347cf978cc64
Benjamin <benkozlovv@gmail.com>
2024-04-23 11:55:54 +0300

    feat: solution

--- 02/sdamgia/46999.py
+++ 02/sdamgia/46999.py
@@ -3,6 +3,6 @@ for x in 0, 1:
     for y in 0, 1:
         for z in 0, 1:
             for w in 0, 1:
-                log = (x == (y <= z)) and (not w <= (x == y))
+                log = (x == (y <= z)) and ((not w) <= (x == y))
                 if log:
                     print(x, y, z, w)
