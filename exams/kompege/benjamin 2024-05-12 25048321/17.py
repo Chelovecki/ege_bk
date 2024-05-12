0cde64587d9a70b69f8cf822acd0aea1b626be94
Benjamin <benkozlovv@gmail.com>
2024-05-12 23:01:04 +0300

    feat: РНО

--- exams/kompege/benjamin 2024-05-12 25048321/17.py
+++ exams/kompege/benjamin 2024-05-12 25048321/17.py
@@ -4,7 +4,7 @@ c = m = 0
 
 for r in range(2, len(numbers)):
     a = numbers[r - 2:r + 1]
-    if len([num for num in a if len(str(num)) == 4 and num % 2 == 0]) <= 1:
+    if len([num for num in a if len(str(abs(num))) == 4 and num % 2 == 0]) <= 1:
         if sum(a) <= max_ends_121:
             c += 1
             m = max(m, sum(a))
