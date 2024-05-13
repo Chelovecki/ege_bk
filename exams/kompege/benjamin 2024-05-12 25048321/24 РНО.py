3f71c8023945a9e19fb7adc10bc61e17011e9c5e
Benjamin <benkozlovv@gmail.com>
2024-05-13 22:29:59 +0300

    updates

--- exams/kompege/benjamin 2024-05-12 25048321/24 РНО.py
+++ exams/kompege/benjamin 2024-05-12 25048321/24 РНО.py
@@ -1,7 +1,7 @@
 s = open('24.txt').read()
 
 c = m = 0
-for r in range(0, len(s) - 3, 3):
+for r in range(1, len(s) - 3, 3):
     a = s[r] + s[r + 1] + s[r + 2]
     if a == 'NPO' or a == 'PNO':
         c += 1
