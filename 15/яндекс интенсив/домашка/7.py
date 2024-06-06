5b25435b6df1e282a399a99698cb8603c3d5d69b
Benjamin <benkozlovv@gmail.com>
2024-06-06 20:07:06 +0300

    feat: add marker that idk how to solve

--- 15/яндекс интенсив/домашка/7.py
+++ 15/яндекс интенсив/домашка/7.py
@@ -1,14 +1,15 @@
-def f(a1, a2, x):
-    return (x not in [14, 16, 18, 20, 22, 24]) or ((x not in [16, 19, 22, 25, 28]) <= x in range(a1, a2))
+def f(a, x):
+    first = [14, 16, 18, 20, 22, 24]
+    second = [16, 19, 22, 25, 28]
+    return (x not in first) or ((x not in second) <= (x in a))
 
+a=range(1,25)
+res = set()
+if all(f(a, x) for x in range(-1000, 1000)):
+        curr_proizv = 1
+        for num in a:
+            curr_proizv *= num
+        res.add(curr_proizv)
+print(min(res))
 
-m = 1e+100
-
-for a1 in range(1000):
-    for a2 in range(a1, 1000):
-        if all([f(a1, a2, x) for x in range(1000)]):
-            curr_proizv = 1
-            for num in range(a1, a2):
-                curr_proizv *= num
-            m = min(m, curr_proizv)
-print(m)
+#todo
